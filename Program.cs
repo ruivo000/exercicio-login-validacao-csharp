221b3e8da5ea2b99bc0344d235b1f46f07785cc9
Gustavo Ferreira <gflr1998@hotmail.com>
2026-01-27 19:37:17 -0300

    Adicionar README e o código

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Linq;
+
+Console.WriteLine("Digite seu login:");
+string login = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(login) || !login.All(char.IsLetter))
+{
+    Console.WriteLine("Login inválido. Use apenas letras e não deixe vazio.");
+    return;
+}
+Console.WriteLine("Login válido");
+Console.WriteLine("Digite sua senha(APENAS NÚMEROS)");
+bool senhaValida = int.TryParse(Console.ReadLine(), out int senha);
+
+if (!senhaValida)
+{
+    Console.WriteLine("Erro: Digite apenas números.");
+    return;
+}
+else{
+    Console.WriteLine("Senha válida!");
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Exercício – Login e Senha (C#)
+
+Programa em C# Console Application que realiza validação de login e senha.
+
+## Funcionalidades
+- Login aceita apenas letras
+- Senha aceita apenas números
+- Validação segura usando TryParse e char.IsLetter
+
+## Tecnologias
+- C#
+- .NET
+- Console Application
+
+## Objetivo
+Praticar lógica de programação e boas práticas em C#.
